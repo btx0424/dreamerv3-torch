ed70b45150cc9f3d9595a5ced6c82278cc443908
btx0424 <btx0424@outlook.com>
2024-08-28 05:17:49 -0400

    update training

--- train_gensim.py
+++ train_gensim.py
@@ -17,83 +17,36 @@ from pprint import pprint
 
 from torchvision.io import write_video
 from torchvision.utils import make_grid
+from torchvision.transforms.functional import resize
+from gen_diversity.dataset.gensim import GensimDataset
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+GENSIM_ROOT = os.environ.get("GENSIM_ROOT")
+IMAGE_SIZE = (128, 128)
 
-class GensimDataset(Dataset):
-
-    def __init__(self, data: TensorDict, seq_length: int):
-        super().__init__()
-        self.data = data
-        self.total_length = data.shape[0]
-        self.seq_length = seq_length
-    
-    def __len__(self):
-        return self.total_length - self.seq_length
-    
-    def __getitem__(self, index):
-        return self.data[index: index+self.seq_length]
-
-    @classmethod
-    def make(cls, root_path, seq_length=20):
-        tasks = os.listdir(root_path)
-        file_paths = []
-        episode_lengths = []
-        for task in tasks:
-            task_path = os.path.join(root_path, task)
-            file_names = [filename for filename in os.listdir(task_path) if filename.endswith(".pt")]
-            for file_name in file_names:
-                file_paths.append(os.path.join(task_path, file_name))
-                episode_lengths.append(int(file_name[:-3].split("_")[-1]))
-        
-        total_length = sum(episode_lengths)
-        
-        logging.info(f"Loading {total_length} steps from {len(tasks)} tasks")
-
-        data = TensorDict({
-            "action": MemoryMappedTensor.empty(total_length, 6),
-            "state": MemoryMappedTensor.empty(total_length, 12),
-            "image": MemoryMappedTensor.empty(total_length, 240, 320, 4),
-            "is_first": MemoryMappedTensor.empty(total_length),
-            "is_terminal": MemoryMappedTensor.empty(total_length),
-        }, [total_length])
-
-        data.memmap_(os.path.join(root_path, "memmaped"))
-        
-        i = 0
-        t = tqdm(total=total_length)
-        for file_path in file_paths:
-            episode_data = torch.load(file_path)
-            episode_len = episode_data.pop("length")
-            episode_data = TensorDict({
-                k: torch.as_tensor(v) for k, v in episode_data.items()
-            }, [episode_len])
-            data[ i: i+episode_len] = episode_data
-            del episode_data
-            i += episode_len
-            t.update(episode_len)
-        return cls(data, seq_length)
-    
-    @classmethod
-    def load(cls, root_path, seq_length=20):
-        data = TensorDict.load_memmap(os.path.join(root_path, "memmaped"))
-        return cls(data, seq_length)
+if GENSIM_ROOT is None:
+    raise ValueError("Please set GENSIM_ROOT environment variable")
 
 
-@hydra.main(config_path=FILE_PATH, config_name="train")
+@hydra.main(config_path=os.path.join(FILE_PATH, "cfg"), config_name="train")
 def main(cfg):
     device = cfg.world_model.device
 
-    run = wandb.init(project="GensimEval", entity="btx0424")
+    run = wandb.init(
+        project="GensimEval", 
+        entity="btx0424",
+        mode=cfg.wandb.mode,
+    )
+    run.config["project"] = "gensim"
 
     model = WorldModel(
         obs_space=gym.spaces.Dict(
             {
-                "image": gym.spaces.Box(0, 255, (240, 320, 3), dtype=np.uint8),
+                "image": gym.spaces.Box(0, 255, (*IMAGE_SIZE, 3), dtype=np.uint8),
                 "state": gym.spaces.Box(0, 20, (12,), dtype=np.float32),
             }
         ),
-        act_space=None,
+        act_space=gym.spaces.Box(-1, 1, (6,), dtype=np.float32),
         step=0,
         config=cfg.world_model,
     ).to(device)
@@ -105,14 +58,12 @@ def main(cfg):
         del ckpt
 
 
-    data_dir = os.path.join(os.environ.get("GENSIM_ROOT"), "data")
-    if data_dir is None:
-        raise ValueError
+    data_dir = os.path.join(GENSIM_ROOT, "data", "train")
 
     try:
-        dataset = GensimDataset.load(data_dir, 40)
+        dataset = GensimDataset.load(data_dir, 40, high_level=False)
     except:
-        dataset = GensimDataset.make(data_dir, 40)
+        dataset = GensimDataset.make(data_dir, 40, high_level=False)
     
 
     dataloader = DataLoader(
@@ -123,19 +74,23 @@ def main(cfg):
     )
 
     for epoch in range(cfg.get("epochs", 10)):
-        for i, data in enumerate(dataloader):
+        for i, data in enumerate(tqdm(dataloader)):
             # some manual processing
             data = data.to(device)
+            data["image"] = resize(data["image"].flatten(0, 1), IMAGE_SIZE).unflatten(0, data.shape)
+            data["image"] = data["image"].permute(0, 1, 3, 4, 2)
             data["image"] = data["image"][..., :3] / 255.0
-            data["cont"] = (1.0 - data["is_terminal"]).unsqueeze(-1)
+            data["cont"] = (1.0 - data["is_terminal"].float()).unsqueeze(-1)
+            data["is_first"] = data["is_first"].float()
 
             post, context, metrics = model._train(data)
             
             metrics = {k: np.mean(v) for k, v in metrics.items()}
-            print(f"Epoch {epoch}, iteration {i}")
-            pprint(metrics)
-            
-            if (i + 1) % 200 == 0:
+            if i % 20 == 0:
+                print(f"Epoch {epoch}, iteration {i}")
+                pprint(metrics)
+
+            if (i + 1) % 500 == 0:
                 with torch.no_grad():
                     results = model.video_pred(data).cpu() * 255.0
                 path = os.path.join(run.dir, f"video-{epoch}-{i}.mp4")
